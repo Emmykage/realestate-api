c3bce05c83dfcee4c2388606b60e002f8f5448f3
morris menanya <emmiemenz@gmail.com>
2023-10-27 19:07:27 +0100

    set up active storage

---
+++ db/migrate/20231027113519_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_no
+      t.string :password_digest
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
