046d02e8fc56db449bd3d502a969856b79925f4e
morris menanya <emmiemenz@gmail.com>
2025-01-04 08:56:20 +0100

    change seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Seeding assets..."
+puts "createing users..."
 
 admin = User.find_or_create_by!(email: 'jondoe@phoenix-realestate.com') do  |user|
     user.first_name = 'jon'
@@ -30,7 +31,7 @@ assets =[
     number_of_bedrooms: 2,
     number_of_bathrooms: 2,
     price: 950000.00,
-    images: "luxury_condo.jpg",
+    images: "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
     status: "active",
     asset_category: "Real Estate",
     city: 1
@@ -44,7 +45,7 @@ assets =[
     number_of_bedrooms: 4,
     number_of_bathrooms: 3,
     price: 3500.00,
-    images: "family_home.jpg",
+    images: " https://images.unsplash.com/photo-1560185127-6a3a9a9b6761",
     status: "active",
     asset_category: "Real Estate",
     city: 2
@@ -58,7 +59,7 @@ assets =[
     number_of_bedrooms: 5,
     number_of_bathrooms: 4,
     price: 2500000.00,
-    images: "beachfront_villa.jpg",
+    images: "https://images.unsplash.com/photo-1556911220-e15b30f91674",
     status: "inactive",
     asset_category: "Real Estate",
     city: 3
@@ -72,7 +73,7 @@ assets =[
     number_of_bedrooms: 1,
     number_of_bathrooms: 1,
     price: 2800.00,
-    images: "modern_apartment.jpg",
+    images: " https://images.unsplash.com/photo-1580587771525-78b9dba3b914",
     status: "active",
     asset_category: "Real Estate",
     city: 4
@@ -86,7 +87,7 @@ assets =[
     number_of_bedrooms: 3,
     number_of_bathrooms: 2,
     price: 450000.00,
-    images: "countryside_cottage.jpg",
+    images: " https://images.unsplash.com/photo-1560448204-e01d514fa481",
     status: "inactive",
     asset_category: "Real Estate",
     city: 5
@@ -100,12 +101,15 @@ assets =[
     number_of_bedrooms: 3,
     number_of_bathrooms: 3,
     price: 12000.00,
-    images: "penthouse_suite.jpg",
+    images: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
     status: "active",
     asset_category: "Real Estate",
     city: 4
   }
 ]
+
+puts "createing assets..."
+
 
 assets.each do |asset_data|
     Asset.find_or_create_by!(name: asset_data[:name]) do |asset|
