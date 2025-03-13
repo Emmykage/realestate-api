1612176c39dfecfbb0388c79f7557ecfaefb95b7
morris menanya <emmiemenz@gmail.com>
2025-03-13 06:30:13 +0100

    skip email confirm

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@ admin = User.find_or_create_by!(email: 'jondoe@phoenix-realestate.com') do  |use
     user.password = 'chemistry'
     user.phone_no = '07064334160'
     user.role = 'admin'
+    user.skip_confirmation_notification! if user.respond_to?(:skip_confirmation_notification!)
+
 
 end
 
@@ -23,6 +25,8 @@ client = User.find_or_create_by!(email: 'yankee@gmail.com') do  |user|
     user.password = 'chemistry'
     user.phone_no = '07064334161'
     user.role = 'client'
+    user.skip_confirmation_notification! if user.respond_to?(:skip_confirmation_notification!)
+
 
 end
 
