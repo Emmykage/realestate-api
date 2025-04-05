ee4ad293720bc0c89dcfd99590c502d8bafa8d7f
morris menanya <emmiemenz@gmail.com>
2025-04-05 07:37:29 +0100

    add host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+
+Rails.application.routes.default_url_options = {
+  host: 'phoenix-api-b176bc85b068.herokuapp.com'
+}
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Email deliver set up
