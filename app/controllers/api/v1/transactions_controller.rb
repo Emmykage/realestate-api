25aa7dc7910bb22470bce280e9871c1d86a4a176
morris menanya <emmiemenz@gmail.com>
2025-06-13 14:48:14 +0100

    update

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -25,7 +25,7 @@ class Api::V1::TransactionsController < ApplicationController
 
   # GET /transactions/1
   def show
-    render json: @transaction
+    render json: {data: @transaction}, status: :ok
   end
 
   # POST /transactions
